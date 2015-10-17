fd2b1b2c91038b1210ce921a60d4d78cabf0815e
Gowrishankarnath <gowrishankar.s.in@ieee.org>
2015-10-17 22:52:34 +0530

    Initial commit

---
+++ Prog_30.py
@@ -0,0 +1,20 @@
+__author__ = 'Dr.S.Gowrishankar'
+
+
+def calculator():
+    total = 0
+    count = 0
+    while True:
+        num = raw_input("Enter a number: ")
+        if num == 'done':
+            print total, count, (total / count)
+            break
+        else:
+            try:
+                total += float(num)
+            except:
+                print "Invalid input"
+                continue
+        count += 1
+
+calculator()
