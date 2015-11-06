d2982b3982a08d2beff3532b58ef72f841ea4c15
Gowrishankarnath <gowrishankar.s.in@ieee.org>
2015-11-07 00:13:19 +0530

    New set of Programs 7/11/2015

--- Prog_30.py
+++ Prog_30.py
@@ -1,7 +1,13 @@
 __author__ = 'Dr.S.Gowrishankar'
 
+#Write a program which repeatedly reads numbers until
+# the user enters 'done'. Once 'done' is entered,
+#print out the total, count, and average of the numbers.
+#If the user enters anything other than a number,
+#detect their mistake using try and except and print
+#an error message and skip to the next number.
 
-def calculator():
+def AverageAndCountFinder():
     total = 0
     count = 0
     while True:
@@ -17,4 +23,4 @@ def calculator():
                 continue
         count += 1
 
-calculator()
+AverageAndCountFinder()
